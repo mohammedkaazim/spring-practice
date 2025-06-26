d713b28a024be4d7af03e48ce8a8e1c8b1226eb0
mohammedkaazim <umohammedkaazim@gmail.com>
2025-06-26 11:46:13 +0530

    IoC Concept with Spring Container

---
+++ src/main/java/com/practice/week1/day3/IoCExample.java
@@ -0,0 +1,16 @@
+package com.practice.week1.day3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.practice.week1.day3")
+public class IoCExample {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(IoCExample.class);
+        Car car = context.getBean(Car.class);
+        car.start();
+    }
+}
