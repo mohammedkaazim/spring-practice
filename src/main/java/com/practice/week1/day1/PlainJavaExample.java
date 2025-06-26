6683fe56e0e1817884634ec16d60f96fda69ba68
mohammedkaazim <umohammedkaazim@gmail.com>
2025-06-26 11:31:03 +0530

    Plain Java Object Creation Example

---
+++ src/main/java/com/practice/week1/day1/PlainJavaExample.java
@@ -0,0 +1,14 @@
+package com.practice.week1.day1;
+
+class Car {
+    public void drive() {
+        System.out.println("Driving the car");
+    }
+}
+
+public class PlainJavaExample {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.drive();
+    }
+}
